50ed859f061d4ce94c4d839857f15e6bca2b4a24
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-09 19:13:20 +0530

    Test case for check Find frequent numbers

---
+++ test/java/MyHashMapTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyHashMapTest
+{
+    @Test
+    public void givenFrequentWordsShouldReturnFrequency()
+    {
+        String sentence = "To Be Or Not To Be";
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words)
+        {
+            Integer value = myHashMap.get(word);
+            if (value == null)
+                value=1;
+            else
+                value=value+1;
+            myHashMap.add(word,value);
+        }
+        int freq = myHashMap.get("to");
+        int freq1 = myHashMap.get("be");
+        System.out.println("Frequently word occurs in \n To Be Or Not To be");
+        System.out.println("To Frequency Occurs - "+freq+" Times");
+        System.out.println("be Frequency Occurs - "+freq1+" Times");
+        Assertions.assertEquals(2,freq);
+        Assertions.assertEquals(2,freq1);
+    }
+}
